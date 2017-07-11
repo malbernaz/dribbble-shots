7877fd837e15d32e0c7062347646a063387babbb
malbernaz <albernazmiguel@gmail.com>
2017-07-11 18:19:04 -0300

    first commit

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-import './style';
-import { Component } from 'preact';
+import "./style";
+
+import { Component } from "preact";
 
 export default class App extends Component {
-	render() {
-		return (
-			<div>
-				<h1>Hello, World!</h1>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+      </div>
+    );
+  }
 }
